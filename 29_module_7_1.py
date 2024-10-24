ec52f103635cea789c56b16de50acbc6f83e299c
jvla73 <jvladimirova73@gmail.com>
2024-10-24 18:09:11 +0300

    Add files via upload

---
+++ 29_module_7_1.py
@@ -0,0 +1,37 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        product = f'{self.name}, {self.weight}, {self.category}'
+        return product
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        str_prod = file.read()
+        file.close()
+        return str_prod
+
+    def add(self, *products):
+        for i in products:
+            if self.get_products().find(f'{i.name}') == -1:
+                file = open(self.__file_name, 'a')
+                file.write(f'{i}\n')
+                file.close()
+            else:
+                print(f'Продукт {i.name} уже есть в магазине')
+
+s1 = Shop()
+p1 = Product('Potato', 5.5, 'Vegetables')
+p2 = Product('Pasta', 0.2, 'Groceries')
+p3 = Product('Apples', 1.5, 'Fruits')
+p4 = Product('Avocado', 2.0, 'Fruits')
+
+s1.add(p1, p2, p3, p4)
+print() # пустая строка, чтобы отделить выводы друг от друга
+print(s1.get_products())
