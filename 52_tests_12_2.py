21952c9fbdc82b37639cdf6d7a19f9d1e655218f
jvla73 <jvladimirova73@gmail.com>
2024-12-20 18:04:24 +0300

    Add files via upload

---
+++ 52_tests_12_2.py
@@ -0,0 +1,82 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        self.name = name
+        self.distance = 0
+        self.speed = speed
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        self.participants.sort(key=lambda x: -x.speed)
+
+        while self.participants:
+            for participant in list(self.participants):
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.usain = Runner(name="Усэйн", speed=10)
+        self.andrey = Runner(name="Андрей", speed=9)
+        self.nick = Runner(name="Ник", speed=3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for key, result in cls.all_results.items():
+            print(f"{key}: {result}")
+
+    def test_usain_nick(self):
+        tournament = Tournament(90, self.usain, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["test_usain_nick"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
+    def test_andrey_nick(self):
+        tournament = Tournament(90, self.andrey, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["test_andrey_nick"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
+    def test_usain_andrey_nick(self):
+        tournament = Tournament(90, self.usain, self.andrey, self.nick)
+        results = tournament.start()
+        TournamentTest.all_results["test_usain_andrey_nick"] = {place: str(runner) for place, runner in results.items()}
+        self.assertTrue(results[max(results.keys())] == "Ник")
+
+
+if __name__ == "__main__":
+    unittest.main()
